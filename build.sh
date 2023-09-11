88f9a17f44f384deac622bfe562ef1fbc924198b
木木的木头 <ok@xjiangwei.cn>
2023-09-11 21:28:45 +0800

    test

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build  -o pmail_linux_amd64  main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build  -o t_linux_amd64  main.go
 
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build  -o pmail_windows_amd64.exe  main.go
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build  -o t_windows_amd64.exe  main.go
 
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build  -o pmail_mac_amd64  main.go
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build  -o t_mac_amd64  main.go
 
-CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build  -o pmail_mac_arm64  main.go
+CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build  -o t_mac_arm64  main.go
 
 # 整理输出文件
 rm -rf output
 mkdir output
 cd output
-mv ../pmail* .
+mv ../t_* .
 cp ../README.md .
