42ba254e144341e9b70fa941ecb091e9f9151fff
木木的木头 <ok@xjiangwei.cn>
2023-09-12 15:37:06 +0800

    移除Build文件

--- build.sh
+++ build.sh
@@ -1,10 +1,5 @@
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build  -o t_linux_amd64  main.go
 
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build  -o t_windows_amd64.exe  main.go
-
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build  -o t_mac_amd64  main.go
-
-CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build  -o t_mac_arm64  main.go
 
 # 整理输出文件
 rm -rf output
